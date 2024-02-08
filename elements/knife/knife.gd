88f204213d3c1be5a2e0370f4b36af48288a8767
Andrew <hydra1337channel@gmail.com>
2024-02-08 22:17:47 +1000

    Lesson 2.2 Втыкание ножей

--- elements/knife/knife.gd
+++ elements/knife/knife.gd
@@ -7,6 +7,20 @@ var speed = 4500.0
 func _physics_process(delta : float):
 	if is_flying:
 		var collision = move_and_collide(speed * delta * Vector2.UP)
+		if collision:
+			handle_collision(collision)
 
 func throw():
 	is_flying = true
+
+func handle_collision(collision : KinematicCollision2D):
+	var collider = collision.get_collider()
+	if collider is Target:
+		add_knife_to_target(collider)
+		is_flying = false
+
+func add_knife_to_target(target : Target):
+	get_parent().remove_child(self)
+	global_position = Target.KNIFE_POSITION
+	rotation = 0
+	target.add_object_with_pivot(self, -target.rotation)
